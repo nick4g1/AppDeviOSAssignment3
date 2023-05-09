11abb2c08fc6702456be6fc35d99e2e6cc923760
JJRCyber <106593073+JJRCyber@users.noreply.github.com>
2023-05-09 15:57:40 +1000

    Initial Commit
    Initial Commit

---
+++ Assignment3/Assignment3/Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3App.swift
+//  Assignment3
+//
+//  Created by Cooper Jacob on 9/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

944f44ec83cdf26d73900beed76afb2e199fe780
JJRCyber <106593073+JJRCyber@users.noreply.github.com>
2023-05-13 14:32:37 +1000

    Add Split screen + friend scroll view

---
+++ Assignment3/Assignment3/View/SplitView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct SplitView: View {
+    @State private var amount = 0.0
+    @State private var selectedFriends: Set<String> = []
+    let friends = Friends().generateFriends()
+    
+    var totalAmount: Double {
+        selectedFriends.count > 0 ? amount / Double(selectedFriends.count) : 0
+    }
+    
+    var body: some View {
+        ZStack {
+            ColorUtils.backgroundColor.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                Text("Split")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                HStack {
+                    Text("Amount")
+                    TextField("Amount", value: $amount, format: .currency(code: "AUD"))
+                        .frame(width: 100)
+                        .background(.white)
+                        .padding()
+                }
+                FriendsScrollView(amount: $amount, selectedFriends: $selectedFriends, friends: friends)
+            }
+        }
+    }
+}
+
+struct SplitView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplitView()
+    }
+}

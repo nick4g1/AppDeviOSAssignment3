9a14b0dc4e3c43f7179c9500a65c178f0421e265
JJRCyber <106593073+JJRCyber@users.noreply.github.com>
2023-05-13 11:59:20 +1000

    Update comments
    Update comments

--- Assignment3/Assignment3/Model/EmailAuthenticationHandler.swift
+++ Assignment3/Assignment3/Model/EmailAuthenticationHandler.swift
@@ -16,26 +16,33 @@ final class EmailAuthenticationHandler {
             return
         }
         
+        // Minimun password for Firebase is 6 characters
         guard password.count > 5 else {
             print("Password is too short")
             return
         }
         
+        // Password validation to check 
         guard password == passwordCheck else {
             print("Passwords do not match")
             return
         }
+        
+        // Try to create user with given email and password
         let returnUserData = try await AuthenticationManager.shared.createUser(email: email, password: password)
         print("Success")
         print(returnUserData)
     }
     
+    // Function to sign in user given email and password
     func signIn(email: String, password: String) async throws {
-        // Add validation here
+        // TODO: Add validation here
         guard !email.isEmpty, !password.isEmpty else {
             print("No email or password found")
             return
         }
+        
+        // Try to sign in user with given email and password
         let returnUserData = try await AuthenticationManager.shared.signInUser(email: email, password: password)
         print("Success")
         print(returnUserData)
--- Assignment3/Assignment3/Model/GoogleSignInHelper.swift
+++ Assignment3/Assignment3/Model/GoogleSignInHelper.swift
@@ -9,6 +9,7 @@ import Foundation
 import GoogleSignIn
 import GoogleSignInSwift
 
+// Custom model with information returned from Google Sign in dialog
 struct GoogleSignInResultModel {
     let idToken: String
     let accessToken: String
@@ -16,23 +17,32 @@ struct GoogleSignInResultModel {
     let email: String?
 }
 
-
+// Helper class for Google Signin
 final class GoogleSignInHelper {
     
+    // This function must run on main thread as it needs access to View
     @MainActor
     func signInGoogle() async throws -> GoogleSignInResultModel {
+        // Get the top VC (Firebase does not have full SwiftUI support yet)
         guard let topVC = ViewUtils.shared.topViewController() else {
             throw URLError(.cannotFindHost)
         }
+        
+        // Get the sign in result object from opened dialog
         let gidSignInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
         
+        // Set idToken from signinresult
         guard let idToken = gidSignInResult.user.idToken?.tokenString else {
+            // FIXME: Should throw custom error
             throw URLError(.badServerResponse)
         }
+        
+        // Set variables for access tokens and name + email
         let accessToken: String = gidSignInResult.user.accessToken.tokenString
         let name = gidSignInResult.user.profile?.givenName
         let email = gidSignInResult.user.profile?.email
         
+        // Returns tokens as a GoogleSignInResultModel struct
         let tokens = GoogleSignInResultModel(idToken: idToken, accessToken: accessToken, name: name, email: email)
         return tokens
     }

104ac3916a8ed7067cd4aec827a445ac3a84f41b
Max Fisher <66274614+maxhfisher@users.noreply.github.com>
2023-05-24 16:26:32 +1000

    implement track view

--- Assignment3/Assignment3/View/TrackView.swift
+++ Assignment3/Assignment3/View/TrackView.swift
@@ -10,23 +10,40 @@ import SwiftUI
 struct TrackView: View {
 	
 	//TODO: Keep track of transfers
+	@StateObject private var viewModel = ProfileModel()
+	
+	let total = 100
 	
     var body: some View {
-		ZStack {
-			ColorUtils.backgroundColor.edgesIgnoringSafeArea(.all)
-			
-			VStack {
-				Spacer()
-				Text("Total Transfers")
-					.font(.headline)
-				Spacer()
-				Text("\(0, format: .currency(code: "AUD"))")
-					.font(.largeTitle)
-					.foregroundColor(.green)
-				Spacer()
+		List {
+			if let user = viewModel.user {
+				Section {
+					HStack {
+						Text("Total Sent")
+						Spacer()
+						Text("\(user.sent, format: .currency(code: "AUD"))")
+							.foregroundColor(.red)
+					}
+					HStack {
+						Text("Total Recieved")
+						Spacer()
+						Text("\(user.recieved, format: .currency(code: "AUD"))")
+							.foregroundColor(.green)
+					}
+					HStack {
+						Text("Net Transfers")
+							.font(.headline)
+						Spacer()
+						Text("\(user.recieved - user.sent < 0 ? "(":"")\(total, format: .currency(code: "AUD"))\(total < 0 ? ")":"")")
+							.foregroundColor(total < 0 ? .red:.green)
+					}
+				}
 			}
 		}
-    }
+		.task {
+			try? await viewModel.loadUser()
+		}
+	}
 }
 
 struct TrackView_Previews: PreviewProvider {

f5ec77e03e8769c2d0e1967e459f3edc61f2b18f
JJRCyber <106593073+JJRCyber@users.noreply.github.com>
2023-05-28 13:32:35 +1000

    Commenting of TransactionManager

--- Assignment3/Assignment3/Model/Firestore/TransactionManager.swift
+++ Assignment3/Assignment3/Model/Firestore/TransactionManager.swift
@@ -10,6 +10,9 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
+// Struct for user transaction - conforms to codable so it can be encoded
+// and decoded for mapping to key values in firestore
+
 struct UserTransaction: Codable {
     let transactionId:String
     let amount:Double
@@ -21,15 +24,19 @@ struct UserTransaction: Codable {
 
 final class TransactionManager {
     
+    // Shared instance of class is declared to be used across application
     static let shared = TransactionManager()
     private init() {}
     
+    // Set transactionCollection to firestore collection "transactions"
     private let transactionCollection = Firestore.firestore().collection("transactions")
 
+    // Retrieves a transaction docuement given user email
     private func transactionDocument(transactionId: String) -> DocumentReference {
         transactionCollection.document(transactionId)
     }
     
+    // Creates a new transaction given a UserTransaction object
     func createNewTransaction(transaction: UserTransaction) async throws {
         try transactionDocument(transactionId: transaction.transactionId).setData(from: transaction, merge: false)
         try await UserManager.shared.addFunds(amount: transaction.amount, email: transaction.recievingAccount)
@@ -37,6 +44,7 @@ final class TransactionManager {
         try await UserManager.shared.addTransaction(transaction: transaction)
     }
     
+    // Retrieves a transaction given a transaction ID
     func getTransaction(transactionId: String) async throws -> UserTransaction {
         try await transactionDocument(transactionId: transactionId).getDocument(as: UserTransaction.self)
         
